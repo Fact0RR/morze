725ca074c4666abece21e1c37a698067a5dc9f74
Artiv <artiv02.2009@mgail.com>
2026-02-18 14:30:13 +0300

    первая версия чата

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+function showText() {
+    const input = document.getElementById("login");
+    let value = input.value;      // то, что ввёл пользователь
+    const textElement = document.querySelector('.messages');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = value;
+    textElement.before(newElement);
+    console.log(newElement);
+}
