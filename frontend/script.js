436d166ea09f80f0550a855fcc347f04cc13e69c
Artiv <artiv02.2009@mgail.com>
2026-02-19 00:47:10 +0300

    пустые сообщения не отправляются

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,12 @@
 function showText() {
     const input = document.getElementById("login");
     let value = input.value;      // то, что ввёл пользователь
-    const textElement = document.querySelector('.messages');
-    const newElement = document.createElement('div');
-    newElement.className = 'mes';
-    newElement.innerHTML = value;
-    textElement.before(newElement);
-    console.log(newElement);
+    if (value !== "") {
+        const textElement = document.querySelector('.messages');
+        const newElement = document.createElement('div');
+        newElement.className = 'mes';
+        newElement.innerHTML = value;
+        textElement.before(newElement);
+        console.log(newElement);
+    }
 }
