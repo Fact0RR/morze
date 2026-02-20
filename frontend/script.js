350b025c399d3196986da6c07b3d2b05e6d7c1a8
Artiv <artiv02.2009@mgail.com>
2026-02-21 00:30:00 +0300

    хуй и вроде ещё чёто

--- frontend/script.js
+++ frontend/script.js
@@ -4,9 +4,25 @@ function showText() {
     if (value !== "") {
         const textElement = document.querySelector('.messages');
         const newElement = document.createElement('div');
-        newElement.className = 'mes';
-        newElement.innerHTML = value;
-        textElement.before(newElement);
-        console.log(newElement);
+        if (value.includes("ХУЙ")) {
+            newElement.className = 'hui';
+            newElement.innerHTML = value;
+            textElement.before(newElement);
+            console.log(newElement);
+        } else {
+            newElement.className = 'mes';
+            newElement.innerHTML = value;
+            textElement.before(newElement);
+            console.log(newElement);
+        }
     }
+    const box = document.getElementById("chatic");
+    box.scrollTop = box.scrollHeight;
 }
+// function clear() {
+//     // получаем все элементы с классом 'mes'
+//     const clearElements = document.querySelectorAll('.mes');
+
+//     // перебираем и удаляем каждый
+//     clearElements.forEach(el => el.remove());
+// }
